46496bfdbcb8bd3897cbc7fba6c1fd2a5267f45a
Azhar omer <azharomer880@gmail.com>
2023-01-27 03:42:53 +0200

    update udagram

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagramapi10581334184-env.eba-ujmdxbep.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
